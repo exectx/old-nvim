3ab4c03a0bf81f5a4dba65921ce81034ba7c46e3
exectx <81214874+exectx@users.noreply.github.com>
2024-02-24 02:29:07 -0500

    add eslint

--- lua/custom/plugins/none-ls.lua
+++ lua/custom/plugins/none-ls.lua
@@ -3,9 +3,11 @@ return {
   config = function()
     local null_ls = require 'null-ls'
     null_ls.setup {
+      debug = true,
       sources = {
         null_ls.builtins.formatting.stylua,
-        -- null_ls.builtins.code_actions.eslint_d,
+        null_ls.builtins.code_actions.eslint_d,
+        null_ls.builtins.diagnostics.eslint_d,
         null_ls.builtins.formatting.prettier,
       },
     }
